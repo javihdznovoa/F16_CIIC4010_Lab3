a80dce5ca9db3b26821bfe5400459b61c0cc019f
4015d7 <4015d7@aguacate.ece.uprm.edu>
2016-08-31 14:27:31 -0400

    Changed Title, size and location.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,13 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Javier Hernandez Novoa");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(900,400);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
-	}
+
+		
+		}
+		
 }
