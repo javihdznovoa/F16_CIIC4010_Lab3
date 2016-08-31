b000416cbc465b15cc1056ff3fd5bc4c90edf413
4015d7 <4015d7@grosella.ece.uprm.edu>
2016-08-31 17:39:57 -0400

    Added Puerto Rico flag

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -5,7 +5,9 @@ public class Lab3Exercises {
 		JFrame myFrame = new JFrame("Javier Hernandez Novoa");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900,400);
-		myFrame.setSize(200, 200);
+		myFrame.setSize(250, 200);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 
 		
--- F16_CIIC4010_Lab3/src/MyPanelClass.java
+++ F16_CIIC4010_Lab3/src/MyPanelClass.java
@@ -24,8 +24,8 @@ public class MyPanelClass extends JPanel {
 		int height = y2 - y1;
 
 		//Paint the background
-		g.setColor(Color.BLUE);
-		g.fillRect(x1, y1, width + 1, height + 1);
+		//g.setColor(Color.BLUE);
+		//g.fillRect(x1, y1, width + 1, height + 1);
 //		 //Draw a border
 //        g.setColor(Color.YELLOW);
 //        g.drawRect(x1, y1, width, height);
@@ -50,6 +50,17 @@ public class MyPanelClass extends JPanel {
 //         p.addPoint(x1 + 15, y1 + 25);
 //         g.setColor(Color.YELLOW);
 //         g.fillPolygon(p);
+		g.setColor(Color.RED);
+		g.fillRect(x1+10, y1+10, 230, 30);
+		g.setColor(Color.WHITE);
+		g.fillRect(x1+10, y1+40, 230, 30);
+		g.setColor(Color.RED);
+		g.fillRect(x1+10, y1+70, 230, 30);
+		g.setColor(Color.WHITE);
+		g.fillRect(x1+10, y1+100, 230, 30);
+		g.setColor(Color.RED);
+		g.fillRect(x1+10, y1+130, 230, 30);
+		
         Polygon p2 = new Polygon();
         p2.addPoint(x1 + 25, y1 + 73);
         p2.addPoint(x1 + 41, y1 + 73);
@@ -61,7 +72,18 @@ public class MyPanelClass extends JPanel {
         p2.addPoint(x1 + 47, y1 + 88);
         p2.addPoint(x1 + 34, y1 + 98);
         p2.addPoint(x1 + 38, y1 + 83);
+        
+        Polygon p3 = new Polygon();
+        p3.addPoint(x1+10,y1+10);
+        p3.addPoint(x1+115,(y1+160)/2);
+        p3.addPoint(x1+10, y1+160);
+        g.setColor(Color.BLUE);
+        g.fillPolygon(p3);
+        
         g.setColor(Color.WHITE);
-        g.drawPolygon(p2);
+        g.fillPolygon(p2);
+        
+        
+        
 	}
 }
